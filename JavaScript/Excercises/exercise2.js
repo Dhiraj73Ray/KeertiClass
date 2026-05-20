400cace710b4909510a064623caf845ff9c3285f
Dhiraj Ray <120560857+Dhiraj73Ray@users.noreply.github.com>
2026-05-20 10:09:41 +0530

    File uploaded

---
+++ JavaScript/Excercises/exercise2.js
@@ -0,0 +1,120 @@
+// Ques.1
+let x = 10;
+let y = 20;
+
+if (x > 5 && y < 25) {
+    console.log("A");
+} else {
+    console.log("B");
+}
+
+// Ques.2
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if (isAdmin || isLoggedIn) {
+//     console.log("Access granted");
+// } else {
+//     console.log("Access denied");
+// }
+
+
+// Ques.3
+// let temp = 30;
+
+// if (!(temp > 25)) {
+//     console.log("Hot");
+// } else {
+//     console.log("Cold");
+// }
+
+
+// Ques.4
+// let a = 0;
+
+// if (a) {
+//     console.log("Truthy");
+// } else {
+//     console.log("Falsy");
+// }
+
+
+
+
+
+// Ques.5
+// let score = 85;
+
+// let grade = (score >= 90) ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : (score >= 60) ? "D" : "Fail";
+
+// console.log(grade);
+
+
+
+
+// Ques.6
+// let loggedIn = true;
+// let hastoken = false;
+
+// let access = (loggedIn && hastoken) ? "Allow" : "Deny";
+// console.log(access);
+
+
+
+
+
+// Ques.7
+// let a = 5;
+// a++;
+// console.log(a);
+
+// let b = 7;
+// ++b;
+// console.log(b);
+
+
+
+
+// Ques.8
+// let x = 10;
+// let y = x++;
+// console.log(x, y);
+
+
+
+
+// Ques.9
+// let p = 5;
+// let q = ++p;
+// console.log(p, q);
+
+
+
+
+// Ques.10
+// let m = 10;
+// console.log(m--);
+// console.log(m);
+
+
+
+
+// Ques.11
+// let n = 5;
+// let result = n++ + ++n + n-- + --n;
+// console.log(result);
+
+
+
+
+// Ques.12
+// let count = 5;
+
+// if (count-- == 5)  {
+//     console.log("Matched");
+// } else {
+//     console.log("Not Matched");
+// }
+
+
+
